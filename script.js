84f7ef43a1de9051fd62fbbd1e7c6dbf8e364bb0
Crafterboy27-school <95499245+Crafterboy27-school@users.noreply.github.com>
2024-04-08 13:31:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,14 @@ document.getElementById("new").onclick=()=>{
   if(a.toLowerCase()[0]==="y")  game()
 }
 game()
+
+          document.addEventListener("keydown",e=>{
+            if(e.ctrlKey&&e.key=="i"){
+(function() {
+    var x = document.createElement("script");
+    x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";
+    x.onload = alert("Loaded Developer Console!");
+    document.head.appendChild(x);
+})()
+            }
+          })
